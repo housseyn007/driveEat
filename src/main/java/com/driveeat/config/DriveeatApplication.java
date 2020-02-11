70e36c7d5f3068f49e7c07ede0f77b7ca765778b
housseyn <attafi-housseyn@hotmail.fr>
2020-02-11 15:48:23 +0100

    initialize the project

---
+++ src/main/java/com/driveeat/config/DriveeatApplication.java
@@ -0,0 +1,13 @@
+package com.driveeat.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveeatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/driveeat/config/DriveeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driveeat.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
