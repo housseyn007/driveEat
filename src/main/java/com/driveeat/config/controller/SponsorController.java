3740f78505aa25b9d48a151dcc6733df79b4cd5f
chaffai13 <abcham2512@gmail.com>
2020-03-03 10:43:58 +0100

    add contact sponsor email

--- src/main/java/com/driveeat/config/controller/SponsorController.java
+++ src/main/java/com/driveeat/config/controller/SponsorController.java
@@ -1,13 +1,17 @@
 package com.driveeat.config.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.driveeat.config.entity.Contact;
 
 @Controller
 public class SponsorController {
 
 	@GetMapping("/sponsors")
-	public String getSponsorPage() {
+	public String getSponsorPage(Model model) {
+		model.addAttribute("contact", new Contact());
 		return "sponsor";
 	}
 }
