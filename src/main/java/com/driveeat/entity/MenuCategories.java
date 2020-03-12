2cc7ec0b05600d9d2d75c7420cbf9d7036bdb9c1
chaffai13 <abcham2512@gmail.com>
2020-03-12 15:58:55 +0100

    create entity /menus/menusCategories/product/categories done

---
+++ src/main/java/com/driveeat/entity/MenuCategories.java
@@ -0,0 +1,58 @@
+package com.driveeat.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "MenuCategories")
+public class MenuCategories {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer menuCategoryId;
+	@NotNull
+	private String name;
+	@NotNull
+	private boolean isVisible;
+	
+	public MenuCategories() {
+		super();
+	}
+
+	public MenuCategories(@NotNull String name, @NotNull boolean isVisible) {
+		super();
+		this.name = name;
+		this.isVisible = isVisible;
+	}
+
+	public Integer getMenuCategoryId() {
+		return menuCategoryId;
+	}
+
+	public void setMenuCategoryId(Integer menuCategoryId) {
+		this.menuCategoryId = menuCategoryId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isVisible() {
+		return isVisible;
+	}
+
+	public void setVisible(boolean isVisible) {
+		this.isVisible = isVisible;
+	}
+
+	
+	
+}
