12b92b61c89ef4190648e57b153846c0a0628f39
chaffai13 <abcham2512@gmail.com>
2020-04-02 17:17:18 +0200

    controller page menu selection

--- src/main/java/com/driveeat/controller/RestaurantController.java
+++ src/main/java/com/driveeat/controller/RestaurantController.java
@@ -1,15 +1,11 @@
 package com.driveeat.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.driveeat.entity.MenuCategories;
-import com.driveeat.entity.Menus;
 import com.driveeat.repository.MenusRepository;
 import com.driveeat.repository.RestaurantsRepository;
 
@@ -21,14 +17,12 @@ public class RestaurantController {
 	@Autowired
 	private MenusRepository menusRepository;
 
-
-
-	@GetMapping("/restaurants")
+	@GetMapping("/restaurantSpecialites/restaurants")
 	public String getRestaurantPage(Model model, @RequestParam Integer id) {
 		model.addAttribute("restaurant", restaurantsRepository.getOne(id));
 		model.addAttribute("menus", menusRepository.findByRestaurants(restaurantsRepository.getOne(id)));
-        model.addAttribute("categories", menusRepository.findByMenuCategories(id));
-        
+		model.addAttribute("categories", menusRepository.findByMenuCategories(id));
+
 		return "restaurant";
 	}
 
