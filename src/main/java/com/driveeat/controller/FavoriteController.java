1377c1851a4b22f94583acedef0b63282ec73c77
housseyn007 <attafi-housseyn@hotmail.fr>
2020-07-22 15:06:38 +0200

    solution of compare a list with an element done

--- src/main/java/com/driveeat/controller/FavoriteController.java
+++ src/main/java/com/driveeat/controller/FavoriteController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.driveeat.entity.Favourites;
@@ -29,22 +28,24 @@ public class FavoriteController {
 	@Autowired
 	private RestaurantsRepository restaurantsRepository;
 	
-	private String listOfIdRestaurantChecked ;
+
 
 	@GetMapping("/utilisateurs/favoris")
 	public String getFavoritePage(Model model) {
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		if (principal instanceof UserPrincipal) {
 			UserPrincipal userPricipal = (UserPrincipal) principal;
-            model.addAttribute("ListOfRestaurantFavorite", favouritesRepository.getRestaurantsFavorites(userPricipal.getUserId()));
-        
+			model.addAttribute("ListOfRestaurantFavorite",
+					favouritesRepository.getRestaurantsFavorites(userPricipal.getUserId()));
+
 		}
 
 		return "favorite";
 	}
 
 	@PostMapping("/restaurantSpecialites/ajout-favoris")
-	public String addFavorite(Model model, Integer restaurantId, Integer specialityId) {
+	public String addFavorite(Model model, Integer restaurantId, Integer specialityId, final RedirectAttributes redirectAttributes) {
+	 
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
 		favourites = new Favourites();
@@ -53,18 +54,11 @@ public class FavoriteController {
 			favourites.setUsers(usersRepository.findById(userPricipal.getUserId()).get());
 			favourites.setRestaurants(restaurantsRepository.findById(restaurantId).get());
 			favouritesRepository.save(favourites);
-		
-			
-			for (Favourites favoriteRestaurant : favouritesRepository.getRestaurantsFavorites(userPricipal.getUserId())) {
-				listOfIdRestaurantChecked += favoriteRestaurant.getRestaurants().getRestaurantId() +" " ;
-			}
-			  
-        
-		}
-		
-		
-		
 
+		}
+		  
+
+       
 		return "redirect:/restaurantSpecialites?id=" + specialityId;
 	}
 }
--- src/main/java/com/driveeat/controller/RestaurantSpecialitesController.java
+++ src/main/java/com/driveeat/controller/RestaurantSpecialitesController.java
@@ -1,13 +1,19 @@
 package com.driveeat.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.driveeat.entity.Favourites;
+import com.driveeat.entity.UserPrincipal;
+import com.driveeat.repository.FavouritesRepository;
 import com.driveeat.repository.RestaurantSpecialitiesRepository;
-import com.driveeat.repository.RestaurantsRepository;
 import com.driveeat.repository.SpecialitiesRepository;
 
 @Controller
@@ -16,11 +22,39 @@ public class RestaurantSpecialitesController {
 	private RestaurantSpecialitiesRepository restaurantSpecialitiesRepository;
 	@Autowired
 	private SpecialitiesRepository specialities;
+	
 
+	@Autowired
+	private FavouritesRepository favouritesRepository;
+
+	private Favourites favourites;
+
+	public List <Integer> listOfIdRestaurantChecked ;
 
 	@GetMapping("/restaurantSpecialites")
 	public String showrestaurents(Model model, @RequestParam Integer id) {
+		
+		listOfIdRestaurantChecked = new ArrayList<Integer>();
+		 
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+		favourites = new Favourites();
+		
+		if (principal instanceof UserPrincipal) {
+			UserPrincipal userPricipal = (UserPrincipal) principal;
+			for (Favourites favoriteRestaurant : favouritesRepository
+					.getRestaurantsFavorites(userPricipal.getUserId())) {
+				listOfIdRestaurantChecked.add(favoriteRestaurant.getRestaurants().getRestaurantId());
+				
+			}
+
+		}
+		
+		
+		
+		model.addAttribute("ListOfIdRestaurantChecked", listOfIdRestaurantChecked);
 		model.addAttribute("RestaurantSpecialites", restaurantSpecialitiesRepository.findBySpecialities(specialities.getOne(id)));
+	     
 		return "restaurantSpecialites";
 	}
 
